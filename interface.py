321c3ec0dcb96e3318d10927c0180dd7d4299033
Flavien54 <flavienb200308@gmail.com>
2026-05-25 15:17:23 -0400

    Update interface.py

--- interface.py
+++ interface.py
@@ -287,7 +287,7 @@ def action_casser_cesar(historique):
 
     if methode == "1":
         # Méthode Chi² — plus fiable sur les textes longs
-        cle, dechiffre = casser_brute.trouver_cle(msg)
+        cle, dechiffre = casser_cesar.trouver_cle(msg)
         nom_methode = "Cassage Cesar (Chi²)"
     else:
         # Méthode naive — basée sur un dictionnaire de mots français courants
